8aa5ba8e29752b633fe2ea3971c6a8e885503bd8
Diego Cantelli <diegocantelli19@gmail.com>
2021-06-20 16:32:41 -0300

    Recebendo os dados que foram emitidos pelo Output

--- ByteBank/src/app/app.component.ts
+++ ByteBank/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ByteBank';
+
+  transferir(transferencia: any){
+    console.log(transferencia.valor, '  ', transferencia.destino);
+  }
 }
