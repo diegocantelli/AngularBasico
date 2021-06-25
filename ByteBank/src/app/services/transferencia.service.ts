5c588a794e219815cb7a121968cb62b78f5dab29
Diego Cantelli <diegocantelli19@gmail.com>
2021-06-25 19:12:38 -0300

    Utilizando serviços para passar valores entre os componentes

--- ByteBank/src/app/services/transferencia.service.ts
+++ ByteBank/src/app/services/transferencia.service.ts
@@ -13,11 +13,20 @@ export class TransferenciaService {
   private listaTransferencia: any[];
 
   constructor() {
-
+    this.listaTransferencia = [];
   }
 
   get transferencias() {
     return this.listaTransferencia;
+  }
+
+  adicionar(transferencia: any){
+    this.hidratar(transferencia);
+    this.listaTransferencia.push(transferencia);
+  }
+
+  private hidratar(transferencia: any) {
+    transferencia.data = new Date();
   }
 
 }
